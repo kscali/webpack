e88a2cde923215c8d8c6fde58ccc574bfa9a6c1c
koy <kosatun@aol.com>
2019-07-30 22:59:11 -0700

    created .eslintrc file

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,22 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extension: ['.js', '.jsx']
   }
 };
